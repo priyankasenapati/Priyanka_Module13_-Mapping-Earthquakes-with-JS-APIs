7d6e4f03d622d0220936b9a02217fee33297c98b
Priyanka Senapati <write2priyankasenapati@gmail.com>
2020-03-17 10:28:15 +0530

    Adding  Mapping_Single_Points/Simple_Map/static/js/logic.js and other files

--- Mapping_Single_Points/Simple_Map/static/js/logic.js
+++ Mapping_Single_Points/Simple_Map/static/js/logic.js
@@ -1,14 +1,21 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	accessToken: API_KEY
+// // Create the map object with a center and zoom level.
+var map = L.map('mapid', {
+    center: [34.0522, -118.2437],
+    zoom: 14,
 });
-// Then we add our 'streets' tile layer to the map.
-streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color: "black",
+    fillColor: "#ffffa1"
+ }).addTo(map);
+
+// Add tile layer that will be the background of our map.
+let tile_url = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}';
+let streets = L.tileLayer(tile_url, {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+})
+    streets.addTo(map);
--- Simple_Map/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = 'pk.eyJ1IjoicHJpeWFua2FzZW5hcGF0aSIsImEiOiJjazd1aDB6MWMxNng5M2ZxdHBoM3pvZmtuIn0.Abwho69-dUb6nRi0eklofQ';
